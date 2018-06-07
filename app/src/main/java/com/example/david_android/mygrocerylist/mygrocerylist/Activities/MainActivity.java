459bbc03e06f0facfd74e2664b34b61b87fb41a0
FazioD <faziod@uw.edu>
2018-06-07 15:06:00 -0700

    added textview and edit/delete feature

--- app/src/main/java/com/example/david_android/mygrocerylist/mygrocerylist/Activities/MainActivity.java
+++ app/src/main/java/com/example/david_android/mygrocerylist/mygrocerylist/Activities/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.david_android.mygrocerylist.mygrocerylist.Activities;
-
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,8 +10,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-
 import com.example.david_android.mygrocerylist.mygrocerylist.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
